2ea536f70a9f6f2672029f72e0bb85704f6de2b7
Tommy Syllow <tommysyllow@gmail.com>
2023-01-03 20:04:04 +0100

    question and answers now load when started

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ let questions = [
     {
         "question": "Wofür steht die Abkürzung HTML?",
         "answer1": "Hypertext Markup Language",
-        "answer2": "Hypertext Markup Language",
-        "answer3": "Hypertext Markup Language",
-        "answer4": "Hypertext Markup Language",
+        "answer2": "Hooker Markup Linguste",
+        "answer3": "Hunt Meat Loop",
+        "answer4": "keine Antwort ist korrekt",
         "rightAnswer": 1
     },
     {
@@ -40,7 +40,21 @@ let questions = [
         "rightAnswer": 1
     }
 ];
+let currentQuestionNumber = 0;
 
 function init() {
-    
+    getTagById('maxQuestions').innerText = questions.length;
+    setupQuestion(currentQuestionNumber);
+}
+
+function getTagById(id) {
+    return document.getElementById(id);
+}
+
+function setupQuestion(currentQuestionNumber) {
+    document.getElementById('question').innerHTML = questions[currentQuestionNumber]["question"];
+    document.getElementById('answer1').innerHTML = questions[currentQuestionNumber]["answer1"];
+    document.getElementById('answer2').innerHTML = questions[currentQuestionNumber]["answer2"];
+    document.getElementById('answer3').innerHTML = questions[currentQuestionNumber]["answer3"];
+    document.getElementById('answer4').innerHTML = questions[currentQuestionNumber]["answer4"];
 }
