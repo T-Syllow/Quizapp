e3bf81705473af4d148b11ecba88600368b551b7
Tommy Syllow <tommysyllow@gmail.com>
2023-01-03 19:41:14 +0100

    Questions Added

---
+++ script.js
@@ -0,0 +1,46 @@
+let questions = [
+    {
+        "question": "Wofür steht die Abkürzung HTML?",
+        "answer1": "Hypertext Markup Language",
+        "answer2": "Hypertext Markup Language",
+        "answer3": "Hypertext Markup Language",
+        "answer4": "Hypertext Markup Language",
+        "rightAnswer": 1
+    },
+    {
+        "question": "Wofür steht die Abkürzung SQL?",
+        "answer1": "Structured Query Language",
+        "answer2": "Hypertext Markup Language",
+        "answer3": "Hypertext Markup Language",
+        "answer4": "Hypertext Markup Language",
+        "rightAnswer": 1
+    },
+    {
+        "question": "Wofür steht die Abkürzung CSS?",
+        "answer1": "Cascading Style Sheet",
+        "answer2": "Hypertext Markup Language",
+        "answer3": "Hypertext Markup Language",
+        "answer4": "Hypertext Markup Language",
+        "rightAnswer": 1
+    },
+    {
+        "question": "Was ist ein Framework?",
+        "answer1": "Ein Baukasten aus fertigen Designs und Funktionen",
+        "answer2": "Hypertext Markup Language",
+        "answer3": "Hypertext Markup Language",
+        "answer4": "Hypertext Markup Language",
+        "rightAnswer": 1
+    },
+    {
+        "question": "Was bildet das HTML Tag <h1></h1> ab?",
+        "answer1": "eine Hauptüberschrift",
+        "answer2": "Hypertext Markup Language",
+        "answer3": "Hypertext Markup Language",
+        "answer4": "Hypertext Markup Language",
+        "rightAnswer": 1
+    }
+];
+
+function init() {
+    
+}
